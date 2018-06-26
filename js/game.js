6dd4bfeb6d302c32d12eaaa15a42391786acbb4e
Colin Matcham <cmatcham@gmail.com>
2018-06-27 08:34:38 +1200

    First commit

---
+++ js/game.js
@@ -0,0 +1,67 @@
+
+/* Game namespace */
+var game = {
+
+    // an object where to store game information
+    data : {
+        // score
+        score : 0
+    },
+
+
+    // Run on page load.
+    "onload" : function () {
+      // Initialize the video.
+      if (!me.video.init(900, 600, {wrapper : "screen", scale : 'auto'})) {
+        alert("Your browser does not support HTML5 canvas.");
+        return;
+      }
+
+      // add "#debug" to the URL to enable the debug Panel
+      if (me.game.HASH.debug === true) {
+        window.onReady(function () {
+            me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+        });
+      }
+
+      // Initialize the audio.
+      me.audio.init("mp3,ogg");
+
+      // Set a callback to run when loading is complete.
+      me.loader.onload = this.loaded.bind(this);
+
+      // Load the resources.
+      me.loader.preload(game.resources);
+
+      // Initialize melonJS and display a loading screen.
+      me.state.change(me.state.LOADING);
+    },
+
+    // Run on game resources loaded.
+    "loaded" : function () {
+        me.pool.register("player", game.Player);
+
+        //me.state.set(me.state.MENU, new game.TitleScreen());
+//        me.state.set(me.state.PLAY, new game.PlayScreen());
+        me.state.set(me.state.MENU, new game.TitleScreen());
+        me.state.set(me.state.PLAY, new game.PlayScreen());
+        //me.state.set(me.state.GAME_OVER, new game.GameOverScreen());
+
+
+        // add our player entity in the entity pool
+        //me.pool.register("mainPlayer", game.PlayerEntity);
+
+        me.pool.register("clumsy", game.BirdEntity);
+        me.pool.register("pipe", game.PipeEntity, true);
+        //me.pool.register("hit", game.HitEntity, true);
+        me.pool.register("ground", game.Ground, true);
+
+        me.input.bindKey(me.input.KEY.SPACE, "fly", true);
+        me.input.bindKey(me.input.KEY.M, "mute", true);
+        me.input.bindPointer(me.input.KEY.SPACE);
+
+        // Start the game.
+        me.state.change(me.state.PLAY);
+        console.log("Me state changed to play");
+    }
+};
---
+++ js/resources.js
@@ -0,0 +1,11 @@
+game.resources = [
+    {name: "bg", type:"image", src: "data/img/bg.png"},
+    {name: "player", type: "image", src: "data/img/plane.png" },
+    {name: "clumsy", type:"image", src: "data/img/plane.png"},
+    {name: "pipe", type:"image", src: "data/img/pipe.png"},
+    {name: "ground", type:"image", src: "data/img/ground.png"},
+    {name: "gameover", type:"image", src: "data/img/gameover.png"},
+    {name: "gameoverbg", type:"image", src: "data/img/gameoverbg.png"},
+    {name: "hit", type:"image", src: "data/img/hit.png"},
+    {name: "getready", type:"image", src: "data/img/getready.png"},
+];
